634c0c26bfb14161ad85e593b90c7a1ec174960a
NikolaiAndrov <nikolaiandrov@abv.bg>
2017-11-09 00:39:47 +0200

    exercises

---
+++ Arrays/SortArrayUsingBubbleSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArrayUsingBubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            bool isSwapped = false;
+
+            do
+            {
+                isSwapped = false;
+
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    var current = numbers[i];
+                    var next = numbers[i + 1];
+
+                    if (current > next)
+                    {
+                        numbers[i + 1] = current;
+                        numbers[i] = next;
+                        isSwapped = true;
+                    }
+                }
+
+            } while (isSwapped);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ Lists/Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var numbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+                numbers.Add(currentNumber);
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
